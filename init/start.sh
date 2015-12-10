d6c25c3590206ef30cbcd072361a466983ab7b75
Stephen Soltesz <soltesz@google.com>
2015-12-09 22:05:51 -0500

    Use simple logger for NDT stdout/stderr.

--- init/start.sh
+++ init/start.sh
@@ -17,28 +17,28 @@ SSL_CERT=/etc/pki/tls/certs/measurement-lab.org.crt
 TLS_PORT=3010
 
 # Location of web100 variables file
-WEB100_VARS=$SLICEHOME/build/ndt/web100_variables 
+WEB100_VARS=$SLICEHOME/build/ndt/web100_variables
 
 # NOTE: explicityly disabled "--adminview" to avoid calculation error bug:
 # https://code.google.com/p/ndt/issues/detail?id=79
-WEB100SRV_OPTIONS="--log_dir $SLICERSYNCDIR/ --snaplog --tcpdump --cputime
+WEB100SRV_OPTIONS="-dddddd --log_dir $SLICERSYNCDIR/ --snaplog --tcpdump --cputime
                    --multiple --max_clients=40 --disable_extended_tests
                    -f $WEB100_VARS"
 FAKEWWW_OPTIONS=""
 
 # Set SSL flags if private key and certificate exist
 if [ -f $PRIVATE_KEY ] && [ -f $SSL_CERT ]; then
-	SSL_OPTIONS="--tls_port $TLS_PORT --private_key $PRIVATE_KEY --certificate $SSL_CERT"
-	WEB100SRV_OPTIONS="${WEB100SRV_OPTIONS} $SSL_OPTIONS"
+    SSL_OPTIONS="--tls_port $TLS_PORT --private_key $PRIVATE_KEY --certificate $SSL_CERT"
+    WEB100SRV_OPTIONS="${WEB100SRV_OPTIONS} $SSL_OPTIONS"
 fi
 
 if ! pgrep -f ndtd &> /dev/null ; then
     echo "Starting ndtd:"
     # rotate log file before starting
     [ -f $logpath/web100srv.log.1 ] && mv $logpath/web100srv.log.1 $logpath/web100srv.log.2
-    [ -f $logpath/web100srv.log   ] && mv $logpath/web100srv.log $logpath/web100srv.log.1 
+    [ -f $logpath/web100srv.log   ] && mv $logpath/web100srv.log $logpath/web100srv.log.1
     # ndtd must run as root
-    nohup $path/ndtd $WEB100SRV_OPTIONS > /dev/null 2>&1 &
+    nohup $path/ndtd $WEB100SRV_OPTIONS 2>&1 | $SLICEHOME/init/logger.py /var/log/web100srv.debug &
     touch /var/lock/subsys/ndtd
 fi
 
