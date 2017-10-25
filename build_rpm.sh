d11100acafad7c80c769a51dee33bc9d05a9c04f
Stephen Soltesz <soltesz@google.com>
2017-10-25 12:20:05 -0400

    Use standard building directory.

--- build_rpm.sh
+++ build_rpm.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 USAGE="$0 'command to run in builder'"
-KEYFILE=${1:?Please provide a comment to run: $USAGE}
+_=${1:?Please provide a comment to run: $USAGE}
 docker pull measurementlab/builder:production-1.0
-docker run -it -v `pwd`:/root/iupui_ndt \
-    measurementlab/builder:production-1.0 bash -c "$@"
+docker run -it -v `pwd`:/root/building \
+    measurementlab/builder:production-1.0 bash -c "cd building; $@"
