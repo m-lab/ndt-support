3b80282d5dd8a052336718e2071107a9c4cd2b22
Stephen Soltesz <soltesz@google.com>
2017-10-25 13:16:54 -0400

    Fix directory

--- build_rpm.sh
+++ build_rpm.sh
@@ -7,4 +7,4 @@ USAGE="$0 'command to run in builder'"
 _=${1:?Please provide a comment to run: $USAGE}
 docker pull measurementlab/builder:production-1.0
 docker run -it -v `pwd`:/root/building \
-    measurementlab/builder:production-1.0 bash -c "cd building; $@"
+    measurementlab/builder:production-1.0 bash -c "cd /root/building; $@"
