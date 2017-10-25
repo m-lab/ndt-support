0c4aaa66872d6f614c3c6fa25efc8d7f9759e73d
Stephen Soltesz <soltesz@google.com>
2017-10-25 14:25:07 -0400

    Fix typo

--- build_rpm.sh
+++ build_rpm.sh
@@ -15,7 +15,7 @@ set -x
 set -e
 
 USAGE="$0 'command to run in builder'"
-_=${1:?Please provide a comment to run: $USAGE}
+_=${1:?Please provide a command to run: $USAGE}
 docker pull measurementlab/builder:production-1.0
 docker run -it -v `pwd`:/root/building \
     measurementlab/builder:production-1.0 bash -c "cd /root/building; $@"
