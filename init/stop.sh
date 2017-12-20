b2fe31fc95277a30d9605409393128603a8415be
Nathan Kinkade <nath@nkinka.de>
2017-12-20 10:03:34 -0700

    Modifies stop.sh to kill all ndtd processes in the PGID of the parent PID such that all children get killed along with the parent.

--- init/stop.sh
+++ init/stop.sh
@@ -5,25 +5,25 @@ source /etc/mlab/slice-functions
 
 if test -f /var/lock/subsys/ndtd ; then
     echo "Stopping ndtd:"
-    kill -KILL `cat /var/lock/subsys/ndtd`
+    kill -KILL -$(cat /var/lock/subsys/ndtd)
     rm -f /var/lock/subsys/ndtd
 fi
 
 if test -f /var/lock/subsys/fakewww ; then
     echo "Stopping fakewww:"
-    kill -TERM `cat /var/lock/subsys/fakewww`
+    kill -TERM $(cat /var/lock/subsys/fakewww)
     rm -f /var/lock/subsys/fakewww
 fi
 
 if test -f /var/lock/subsys/flashpolicyd.py ; then
     echo "Stopping flashpolicyd.py:"
-    kill -TERM `cat /var/lock/subsys/flashpolicyd.py`
+    kill -TERM $(cat /var/lock/subsys/flashpolicyd.py)
     rm -f /var/lock/subsys/flashpolicyd.py
 fi
 
 if test -f /var/lock/subsys/inotify_exporter ; then
     echo "Stopping inotify_exporter:"
-    kill -TERM `cat /var/lock/subsys/inotify_exporter`
+    kill -TERM $(cat /var/lock/subsys/inotify_exporter)
     rm -f /var/lock/subsys/inotify_exporter
 fi
 EOF
