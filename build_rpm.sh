0ef70db422eb6014bceaacdba54bf13bae7ad02c
Nathan Kinkade <nath@nkinka.de>
2017-12-21 15:14:44 -0700

    Removes redirection of RPM build from build_rpm.sh and adds it to .travis.yml... will be hidden if exit is 0, or log will get catted if fails.

--- build_rpm.sh
+++ build_rpm.sh
@@ -18,5 +18,4 @@ USAGE="$0 'command to run in builder'"
 _=${1:?Please provide a command to run: $USAGE}
 docker pull measurementlab/builder:production-1.0
 docker run -it -v `pwd`:/root/building \
-    measurementlab/builder:production-1.0 bash -c "cd /root/building; $@" > \
-    /dev/null
+    measurementlab/builder:production-1.0 bash -c "cd /root/building; $@"
